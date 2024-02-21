7572302d7df1943749fc04ee091a14bd299e94a5
TimBuck1 <thehindurules@gmail.com>
2024-02-21 17:17:16 +0530

    Update Subset.java

--- Subset.java
+++ Subset.java
@@ -1,71 +1,24 @@
-package map;
-
 import java.math.BigDecimal;
-import java.util.*;
-import java.util.stream.Collectors;
 
-public class GroupListToMap {
+public class MyObject implements Comparable<MyObject> {
 
-    public static void main(String[] args) {
-        List<YourObject> objects = getYourObjects(); // Replace with your actual list of objects
+    private BigDecimal field1;
+    private BigDecimal field2;
 
-        Map<Long, List<YourObject>> maxUpperBandByGroup = objects.stream()
-                .collect(Collectors.groupingBy(
-                        YourObject::getGroupId,
-                        Collectors.collectingAndThen(
-                                Collectors.toList(),
-                                list -> list.stream()
-                                        .sorted(Comparator.comparing(YourObject::getCost, Comparator.nullsFirst(Comparator.naturalOrder())))
-                                        .collect(Collectors.toList())
-                        )
-                ));
+    // Constructors, getters, setters, etc.
 
-        // Print the result map
-        maxUpperBandByGroup.forEach((groupId, objectsWithSortedUpperBand) ->
-                System.out.println("Group: " + groupId + ", Sorted cost Objects: " + objectsWithSortedUpperBand)
-        );
+    @Override
+    public int compareTo(MyObject other) {
+        // Compare field1 first
+        int field1Comparison = this.field1.compareTo(other.field1);
+
+        if (field1Comparison != 0) {
+            return field1Comparison;
+        }
+
+        // If field1 is equal, compare field2
+        return this.field2.compareTo(other.field2);
     }
 
-    // Other methods...
-
-    // Replace this method with your actual list of objects
-    private static List<YourObject> getYourObjects() {
-        // Implementation to create and return a list of YourObject instances
-        return Arrays.asList(
-                new YourObject(1L, null),
-                new YourObject(1L, BigDecimal.valueOf(15.5)),
-                new YourObject(1L, BigDecimal.valueOf(15.2)),
-                new YourObject(2L, BigDecimal.valueOf(12.8)),
-                new YourObject(2L, null),
-                new YourObject(3L, BigDecimal.valueOf(8.0)),
-                new YourObject(3L, BigDecimal.valueOf(9.5)),
-                new YourObject(3L, null)
-        );
-    }
-
-    static class YourObject {
-        private final Long groupId;
-        private final BigDecimal cost;
-
-        public YourObject(Long groupId, BigDecimal upperBand) {
-            this.groupId = groupId;
-            this.cost = upperBand;
-        }
-
-        public Long getGroupId() {
-            return groupId;
-        }
-
-        public BigDecimal getCost() {
-            return cost;
-        }
-
-        @Override
-        public String toString() {
-            return "YourObject{" +
-                    "groupId=" + groupId +
-                    ", cost=" + cost +
-                    '}';
-        }
-    }
+    // Other methods and fields
 }
