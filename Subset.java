700aade131e52dbf4642dbcde99498d3c44dbd89
TimBuck1 <thehindurules@gmail.com>
2024-02-21 17:25:52 +0530

    Update Subset.java

--- Subset.java
+++ Subset.java
@@ -1,11 +1,14 @@
 import java.math.BigDecimal;
+import java.util.TreeSet;
 
-public class MyObject implements Comparable<MyObject> {
-
+class MyObject implements Comparable<MyObject> {
     private BigDecimal field1;
     private BigDecimal field2;
 
-    // Constructors, getters, setters, etc.
+    public MyObject(BigDecimal field1, BigDecimal field2) {
+        this.field1 = field1;
+        this.field2 = field2;
+    }
 
     @Override
     public int compareTo(MyObject other) {
@@ -17,8 +20,29 @@ public class MyObject implements Comparable<MyObject> {
         }
 
         // If field1 is equal, compare field2
-        return this.field2.compareTo(other.field2);
+        int field2Comparison = this.field2.compareTo(other.field2);
+
+        // Ensure that even if field2 is equal, the result is non-zero
+        return field2Comparison == 0 ? 1 : field2Comparison;
     }
 
-    // Other methods and fields
+    @Override
+    public String toString() {
+        return "MyObject{" +
+                "field1=" + field1 +
+                ", field2=" + field2 +
+                '}';
+    }
+}
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        TreeSet<MyObject> myObjects = new TreeSet<>();
+
+        myObjects.add(new MyObject(new BigDecimal("10"), new BigDecimal("20")));
+        myObjects.add(new MyObject(new BigDecimal("5"), new BigDecimal("30")));
+        myObjects.add(new MyObject(new BigDecimal("10"), new BigDecimal("10")));
+
+        System.out.println("Sorted Set: " + myObjects);
+    }
 }
