6db97db39dda36a4c9315af9fba2e759a3840257
TimBuck1 <thehindurules@gmail.com>
2024-06-07 15:49:50 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,16 @@
-private static String createInsertSql(String template, String[] values) {
-        for (String value : values) {
+    private static String createInsertSql(String template, String[] columns, String[] values) {
+        for (int i = 0; i < values.length; i++) {
+            String value = values[i].trim();
             if ("\"\"".equals(value)) {
                 template = template.replaceFirst("\\?", "NULL");
             } else {
                 value = value.replaceAll("^\"|\"$", ""); // Remove surrounding double quotes if any
-                template = template.replaceFirst("\\?", "'" + value + "'");
+                if (isDateColumn(columns[i])) {
+                    value = "TO_DATE('" + value + "', 'DD-MON-YY')";
+                } else {
+                    value = "'" + value + "'";
+                }
+                template = template.replaceFirst("\\?", value);
             }
         }
         return template;
