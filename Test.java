204236ed68f59415240fbe6a3513134a6679283a
TimBuck1 <thehindurules@gmail.com>
2024-12-02 19:48:50 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,33 @@
-WHERE NOT EXISTS (
-        SELECT 1
-        FROM entity e
-        WHERE e.bid = opr.trade
-          AND e.type = 'O'
-          AND e.action <> 'delete'
-    )
+WITH MODIFIED_alpha AS (
+    -- Identify new or modified alphas
+    SELECT tr.id AS alphaid
+    FROM alpha tr
+    LEFT JOIN charge ch ON tr.id = ch.alphaid
+    WHERE ch.alphaid IS NULL OR tr.uuid <> ch.uuid
+),
+COMBINED_DATA AS (
+    -- Fetch alpha-related records from beta, gamma, and delta
+    SELECT mt.alphaid, 'alpha' AS source, tr.*
+    FROM MODIFIED_alpha mt
+    JOIN alpha tr ON mt.alphaid = tr.id
+    
+    UNION ALL
+    
+    SELECT mt.alphaid, 'beta' AS source, beta.*
+    FROM MODIFIED_alpha mt
+    JOIN option beta ON mt.alphaid = beta.alpha
+    
+    UNION ALL
+    
+    SELECT mt.alphaid, 'gamma' AS source, gamma.*
+    FROM MODIFIED_alpha mt
+    JOIN future gamma ON mt.alphaid = gamma.alpha
+    
+    UNION ALL
+    
+    SELECT mt.alphaid, 'delta' AS source, ofs.*
+    FROM MODIFIED_alpha mt
+    JOIN delta ofs ON mt.alphaid = ofs.alpha
+)
+
+SELECT * FROM COMBINED_DATA;
